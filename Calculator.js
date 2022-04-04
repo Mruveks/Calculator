76e7f5432e1cad2d7c17adc7032b51fcc407c840
Kuba Mrowiec <mruvekss@gmail.com>
2022-04-04 15:16:38 +0200

    Results Update

--- Calculator.js
+++ Calculator.js
@@ -1,27 +1,45 @@
-let dislpay = document.getElementById('display');
+let display = document.getElementById('display');
+let results = document.getElementById('last-result');
 let buttons = Array.from(document.getElementsByClassName('grid-button'));
+let refresh = document.getElementById('refresh-results');
+
+var resultsArray = []; 
+resultsArray.length = 17;
+
+function checkErrors() {
+  if(display.innerText === 'Error' || display.innerText === 'undefined' || display.innerText === 'NaN'){
+    display.innerText = '';
+    return true;
+  } 
+  else return false;
+}
 
 buttons.map( button => {
   button.addEventListener('click', (e) => {
 
     switch(e.target.innerText){ 
         case 'AC':
-            dislpay.innerText = '';
+            display.innerText = '';
         break;
 
         case '←':
-            if(display.innerText){
-              display.innerText = display.innerText.slice(0, -1); 
-            }
+          if(checkErrors() === true ){
+            return null;}
+              else display.innerText = display.innerText.slice(0, -1);             
         break;
 
         case '=':
             try{
-            if(display.innerText === 'Error'){
-                display.innerText = '';
-            }
-            else
-                dislpay.innerText = eval(display.innerText); 
+              if(checkErrors() === true ){
+                return null;
+              }
+              else
+                checkErrors();
+                display.innerText = eval(display.innerText);  
+                resultsArray.unshift(display.innerText);
+                results.innerText += resultsArray[0] + '\n'; 
+                  
+                  
             } catch{
             display.innerText = 'Error'
             }
@@ -30,9 +48,15 @@ buttons.map( button => {
       default:
           display.innerText += e.target.innerText;
 
-          if(innerText.length>=30){
+          if(display.innerText.length>=30){
               display.innerText = '';
           }
     }
   });
 });
+
+refresh.addEventListener('click', (e) =>{
+  results.innerText = '';
+  resultsArray.splice(0,resultsArray.length);
+  console.log(resultsArray);
+})
